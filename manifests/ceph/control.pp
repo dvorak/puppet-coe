29c862211129145cb156fe1d87c8bff3d98953ea
dotalton <dotalton@cisco.com>
2013-08-21 09:27:00 -0700

    removed ceph::conf declaration, it conflicted with ceph_common in core.pp merged the glance class into the control class delete the glance class

--- manifests/ceph/control.pp
+++ manifests/ceph/control.pp
@@ -13,32 +13,30 @@ class coe::ceph::control(
     require => Apt::Source['ceph'],
   }
 
-  class { 'ceph::conf':
-    fsid      => $fsid,
-  }
-
   file { '/etc/ceph/client.admin':
     ensure  => present,
     mode    => 0644,
-    require => Exec['copy the admin key to make glance work'],
+    require => Exec['copy the admin key'],
   }
 
   file { '/etc/ceph/keyring':
     mode    => 0644,
-    require => Exec['copy the admin key to make glance work'],
+    require => Exec['copy the admin key'],
   }
 
-  exec { 'copy the admin key to make glance work':
+  exec { 'copy the admin key':
     command => 'cp /etc/ceph/keyring /etc/ceph/client.admin',
     creates => '/etc/ceph/client.admin',
     require => [ Package['ceph'], Ceph::Key['admin'] ],
   }
 
-  exec { 'create the pool':
-    command => "/usr/bin/ceph osd pool create ${::glance_ceph_pool} 128",
-    unless  => "/usr/bin/rados lspools | grep -sq ${::glance_ceph_pool}",
-    require => Exec['copy the admin key to make glance work'],
-    notify  => [ Service['glance-api'], Service['glance-registry'] ],
+  if $::glance_ceph_enabled {
+    exec { 'create the pool':
+      command => "/usr/bin/ceph osd pool create ${::glance_ceph_pool} 128",
+      unless  => "/usr/bin/rados lspools | grep -sq ${::glance_ceph_pool}",
+      require => Exec['ceph-admin-key'],
+      notify  => [ Service['glance-api'], Service['glance-registry'] ],
+    }
   }
 
 }
