494612e56c8e5165b21e4753bbbf7203ac48eb24
Satya Sanjibani Routray <satroutr@cisco.com>
2013-10-01 21:39:06 +0530

    Added ovs.pp for fixing ovs log flooding issue
    Fixes: Bug#1208979

---
+++ manifests/ovs.pp
@@ -0,0 +1,14 @@
+#
+# This module will expose the functionality for users
+# to alter the log level for open vswitch
+#
+define coe::ovs(
+  $facility    = 'facility',
+  $log_level   = 'log_level',
+){
+  exec {"$name":
+    command  => "/usr/bin/ovs-appctl vlog/set ${name}:${facility}:${log_level}",
+    path     => '/bin:/usr/bin:/sbin:/usr/sbin',
+    require  => Service['openvswitch'],
+  }
+}
